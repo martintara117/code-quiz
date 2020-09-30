609c542add0371a9b1234f78505eb4b4dcd29eca
Tara Martin <taramartin@Taras-MacBook-Pro.local>
2020-09-30 18:06:41 -0400

    added code to js. Created a quiz const with the quiz  questions and answers put into an array. created variables for. outlines what each variable will be used for in subtext. created subtext for future code.

---
+++ script.js
@@ -0,0 +1,60 @@
+// DATA
+const quiz = [
+  {
+    q: "Commonly used data types DO NOT include:",
+    a: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    correct: "3. alerts",
+  },
+  {
+    q: "The condition is an if/else statement when enclosed within____.",
+    a: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parentheses",
+      "4. square brackets",
+    ],
+    correct: "3. parentheses",
+  },
+  {
+    q: "Arrays in JavaScript can be used to store____.",
+    a: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above",
+    ],
+    correct: "4. all of the above",
+  },
+  {
+    q:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    a: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    correct: "4. parentheses",
+  },
+  {
+    q:
+      "A very useful tool used during development and debugging for printing content to the debugger:",
+    a: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+    correct: "4. console.log",
+  },
+];
+
+//SETTINGS
+const delayBetweenQuestions = 1500; //2 seconds is 2000 milliseconds
+const maxTime = 75; //in seconds
+const timePenalty = 10; //seconds lost with wrong answer
+
+//APPLICATION VARIABLES
+var numberOfCorrectAnswers;
+var currentQuestionIndex;
+var timer; //an identifier for the setInterval function
+var timeRemaining; //starts at maxTime then decrement by one every second
+
+//DOM ELEMENTS
+var questionSection = document.querySelector("main section:nth-of-type(2)");
+var answerButtons = questionSection.querySelectorAll("button");
+var timeOutput = document.querySelector(".timer"); //<button class="timer">Time Remaining: 0</button>
+
+//SET EVENT LISTENERS/HANDLERS
+//header button
+//APPLICATION LOGIC
